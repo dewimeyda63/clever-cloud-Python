b3dfe9d88aef5faa068b62a61d51cdb8f0ff93b2
MrRize-web <54984438+MrRize-web@users.noreply.github.com>
2021-04-26 14:02:23 +0200

    test

--- MonScript.py
+++ MonScript.py
@@ -1,2 +1,11 @@
-print("Hello World !")
-print("JPP")
+import http.server
+PORT = 8080
+server_address = ("", PORT)
+
+server = http.server.HTTPServer
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["/"]
+print("Serveur actif sur le port :", PORT)
+
+httpd = server(server_address, handler)
+httpd.serve_forever()
